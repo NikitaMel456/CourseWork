9c6e13a3c64d47e75b521a2f904fa3e3543f9683
Nikita <nikita.melentev13@hotmail.com>
2018-05-16 09:15:14 +0300

    Postgres connection works

--- Context/index.js
+++ Context/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var pg = require('pg');
+let pg = require('pg');
 pg.defaults.ssl = true;
 
 module.exports = (Sequelize, config) => {
@@ -9,6 +9,7 @@ module.exports = (Sequelize, config) => {
         logging: false,
         define: {
             timestamps: true,
+
             paranoid: true,
             defaultScope: {
                 where: { deletedAt: { $eq: null }}
@@ -18,8 +19,8 @@ module.exports = (Sequelize, config) => {
     //var db ='postgres://ioometgjahdzsz:f03380b9c7c27ad9a2fd5a9e75438d6e98ddabf90398147b256c494fff109d40@ec2-50-19-232-205.compute-1.amazonaws.com:5432/d2th3jrohudmoi'
     const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, options);
 
-    const User = require('../models/user')(Sequelize, sequelize);
-    const Post = require('../models/post')(Sequelize, sequelize);
+    const User = require('../Models/user')(Sequelize, sequelize);
+    const Post = require('../Models/post')(Sequelize, sequelize);
 
     User.hasMany(Post, {foreignKey: 'authorId'});
     Post.belongsTo(User, {foreignKey: 'authorId', as: 'author'});
