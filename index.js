2858c9dbbc2d28c9de9b34c02ed588e24384a490
Nikita <nikita.melentev13@hotmail.com>
2018-05-16 10:12:01 +0300

    Postgres connection works

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const config = require('./tsconfig.json');
 
 const db = require('./Context')(Sequelize, config);
 const server = require('./server')(db, config);
-
+const port = process.env.PORT || 3000;
 (async function() {
     await db.sequelize.sync();
 
-    server.listen(3000, () => console.log('Server is running'));
+    server.listen(port, () => console.log('Server is running'));
 })();
 
