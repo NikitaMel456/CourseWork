e939953c0631ff59d13a5a7bcec1f7a67787612e
Nikita <nikita.melentev13@hotmail.com>
2018-05-16 09:21:02 +0300

    Postgres connection works

--- Context/index.js
+++ Context/index.js
@@ -19,8 +19,8 @@ module.exports = (Sequelize, config) => {
     //var db ='postgres://ioometgjahdzsz:f03380b9c7c27ad9a2fd5a9e75438d6e98ddabf90398147b256c494fff109d40@ec2-50-19-232-205.compute-1.amazonaws.com:5432/d2th3jrohudmoi'
     const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, options);
 
-    const User = require('../Models/user')(Sequelize, sequelize);
-    const Post = require('../Models/post')(Sequelize, sequelize);
+    const User = require('../Models/User')(Sequelize, sequelize);
+    const Post = require('../Models/Post')(Sequelize, sequelize);
 
     User.hasMany(Post, {foreignKey: 'authorId'});
     Post.belongsTo(User, {foreignKey: 'authorId', as: 'author'});
